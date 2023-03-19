62a9c1995f11084fa7df26804e387dee8679a9c0
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2023-03-18 22:10:16 -0300

    revert

--- src/files.rs
+++ src/files.rs
@@ -4,8 +4,8 @@ use std::io::prelude::*;
 
 pub fn catfile(blobid: &String) -> String {
     let fpath = format!(".git/objects/{}/{}", &blobid[..2], &blobid[2..]);
-    let bdata = fs::read(fpath).unwrap();
+    let file = fs::File::open(fpath).unwrap();
     let mut s = String::new();
-    ZlibDecoder::new(&bdata[..]).read_to_string(&mut s).unwrap();
+    ZlibDecoder::new(file).read_to_string(&mut s).unwrap();
     return s;
 }
